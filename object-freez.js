7e6011972d614b5d73d1bc54040b47b582d9f39f
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2020-03-15 08:34:24 +0600

    Experiment freeze object

---
+++ object-freez.js
@@ -0,0 +1,19 @@
+function freezeObject() {
+  "user strict";
+
+  const MATH = {
+    PI: 3.14
+  }
+
+  Object.freeze(MATH);
+
+  try {
+    MATH.PI = 99;
+  } catch (e) {
+    console.log(e);
+  }
+
+  console.log("Update pi is " + MATH.PI);
+}
+
+freezeObject();
